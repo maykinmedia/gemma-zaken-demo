c68cadbb126ac6a524f4a56a9f899b841d6c7a6b
Joeri Bekker <joeri@maykinmedia.nl>
2018-07-24 15:06:31 +0200

    Added Django-compatible settings to client.

---
+++ src/zdsclient/__init__.py
@@ -0,0 +1,7 @@
+from zdsclient.client import Client
+
+
+ztc_client = Client('ztc')
+zrc_client = Client('zrc')
+orc_client = Client('orc')
+drc_client = Client('drc')
---
+++ src/zdsclient/conf.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+
+
+class Settings(object):
+    config = None
+    base_dir = None
+
+    def __init__(self):
+        self.config = {}
+        self.base_dir = os.path.abspath(os.path.join(
+            os.path.dirname(__file__),
+            os.path.pardir,
+        ))
+
+    def load(self, config_file=None):
+        if config_file is None:
+            config_file = os.path.join(self.base_dir, 'config.yml')
+
+        with open(config_file, 'r') as _config:
+            self.config.update(yaml.safe_load(_config))
+
+        return self.config
+
+
+settings = Settings()
