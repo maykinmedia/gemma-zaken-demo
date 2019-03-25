25a60eddf52c618ae7ead42bfb585342eee6d947
Joeri Bekker <joeri@maykinmedia.nl>
2019-03-25 18:08:23 +0100

    Fixes Hijack issue in admin.

--- src/zac/urls.py
+++ src/zac/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
+    path('hijack/', include('hijack.urls', namespace='hijack')),
     path('demo/', include('zac.demo.urls')),
     path('status/', include('zac.status.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
