167b1558df69c8ad0b9f8e5ed010c03897eaf8b1
annashamray <anna.shamray@maykinmedia.nl>
2020-09-30 12:09:10 +0200

    :sparkles: Ref.https://github.com/maykinmedia/objects-api/issues/49 - add request to objects api into objectdata app

--- src/zac/demo/objectdata/views.py
+++ src/zac/demo/objectdata/views.py
@@ -1,9 +1,11 @@
+import json
 from django import forms
 from django.views.generic import FormView
 
 from zac.demo.mixins import ZACViewMixin
 from zac.demo.models import SiteConfiguration
 from zac.demo.utils import api_response_list_to_dict
+from ..models import SiteConfiguration
 
 
 def get_objecttype_choices():
@@ -18,6 +20,11 @@ def get_objecttype_choices():
 
 
 class ObjectMapForm(forms.Form):
+    # circle params are used to save circle after refreshing the page
+    latitude = forms.FloatField(widget=forms.HiddenInput)
+    longitude = forms.FloatField(widget=forms.HiddenInput)
+    radius = forms.IntegerField(widget=forms.HiddenInput)
+
     coordinates = forms.CharField(widget=forms.HiddenInput)
     objecttype = forms.ChoiceField()
 
@@ -32,3 +39,30 @@ class ObjectMapView(ZACViewMixin, FormView):
     subtitle = 'Toon alle openbare object data'
     template_name = 'demo/objectdata/object_map.html'
     form_class = ObjectMapForm
+
+    def form_valid(self, form):
+        config = SiteConfiguration.get_solo()
+        objects_client = config.objects_api.build_client()
+
+        form_data = form.cleaned_data
+
+        search_data = {
+            'geometry': {
+                'within': {
+                    'type': 'Polygon',
+                    'coordinates': [
+                        json.loads(form_data['coordinates'])
+                    ]
+                }
+            },
+            "type": form_data["objecttype"]
+        }
+
+        objects_data = objects_client.operation('object_search', search_data)
+
+        return self.render_to_response(
+            self.get_context_data(
+                form=form,
+                objects_json=objects_data,
+            )
+        )
